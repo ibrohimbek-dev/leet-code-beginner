f88e60e9fb02297f1b3734741977237524013195
Ibrohimbek-Dev <programming0127@gmail.com>
2024-06-05 09:11:32 +0900

    feat: My initial commit on leet code beginner tasks

---
+++ trains/train1.ts
@@ -0,0 +1,9 @@
+// 2024-06-05
+// TASK
+// Score of a String
+
+// ----------------------------------------------------
+// Task Definition is here:
+// https://leetcode.com/problems/score-of-a-string/description/?envType=daily-question&envId=2024-06-01
+// ----------------------------------------------------
+
