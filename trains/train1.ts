6955daeb8fd4cb4de643048b1b020deba7fee03c
Ibrohimbek-Dev <programming0127@gmail.com>
2024-06-05 11:32:16 +0900

    'Store a String' task is solved

--- trains/train1.ts
+++ trains/train1.ts
@@ -7,3 +7,16 @@
 // https://leetcode.com/problems/score-of-a-string/description/?envType=daily-question&envId=2024-06-01
 // ----------------------------------------------------
 
+const sentence: string = "hello";
+
+const scoreOfString = (text: string) => {
+	let result: number = 0;
+	for (let i = 0; i < text.length - 1; i++) {
+		result += Math.abs(text.charCodeAt(i) - text.charCodeAt(i + 1));
+	}
+
+	return result;
+};
+
+const result = scoreOfString(sentence);
+console.log("result is:", result);
